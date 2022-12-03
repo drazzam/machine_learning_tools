e06246c038085dc750f1a2697bff40fdb61b11d6
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2022-12-03 22:00:43 +0200

    data_splitter.py

--- data_splitter.py
+++ data_splitter.py
@@ -4,13 +4,17 @@ import shutil
 import os
 import sys
 
+# Set the path for the classification csv file
 labels = pd.read_csv(r'C:\Users\Ahmed Azzam\Downloads\Compressed\Training_Set\Training_Set\class.csv')
 
+# Set the path for the images direction
+# Replace "DR" with an empty folder you create inside the direction (it will be the prototype folder for splitting proccess)
 train_dir =r'C:\Users\Ahmed Azzam\Downloads\Compressed\Training_Set\Training_Set\Training'
 DR = r'C:\Users\Ahmed Azzam\Downloads\Compressed\Training_Set\Training_Set\DR'
 if not os.path.exists(DR):
     os.mkdir(DR)
-
+    
+# Replace "DR" with the prototype folder name
 for filename, class_name in labels.values:
     # Create subdirectory with `class_name`
     if not os.path.exists(DR + str(class_name)):
